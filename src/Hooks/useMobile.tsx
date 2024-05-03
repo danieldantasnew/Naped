4df1e9e455749cfb5070735c4103a94c4ea53d3f
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-03 12:25:09 -0300

    feat: criação de Hook que verifica o tamanho da tela

---
+++ src/Hooks/useMobile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const useMobile = (screen: string) => {
+  const [match, setMatch] = React.useState(false);
+
+  React.useEffect(() => {
+    function handleMedia() {
+      const mobile = window.matchMedia(`(max-width: ${screen}`).matches;
+      setMatch(mobile);
+    }
+
+    window.addEventListener('resize', handleMedia);
+    handleMedia()
+    return ()=> window.removeEventListener('resize', handleMedia);
+  }, [screen]);
+
+  return match;
+}
+
+export default useMobile;
