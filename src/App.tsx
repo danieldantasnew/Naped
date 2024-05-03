8e877b51672455af084faf1e353efc34b17fa970
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-03 16:48:20 -0300

    refactor: alterado a forma que o DataContext é chamado

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,23 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/Home";
+import {DataContext} from "./Context/DataContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <section className="contentBody">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="*" element={<div>non se</div>}></Route>
-          </Routes>
-        </main>
-        <Footer/>
-      </section>
+      <DataContext>
+        <section className="contentBody">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="*" element={<div>non se</div>}></Route>
+            </Routes>
+          </main>
+          <Footer />
+        </section>
+      </DataContext>
     </BrowserRouter>
   );
 }
