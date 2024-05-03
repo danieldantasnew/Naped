281a99ceab48b760fa1f0c95d516eabaa85650ab
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-03 12:23:39 -0300

    feat: adicionado estilização do menu mobile

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,16 +1,27 @@
-import Logo from '../../assets/Logo';
-import OptionsMenu from './NavMenu/NavMenu';
-import Account from './Account/Account';
-import styles from './Header.module.css';
+import React from "react";
+import Logo from "../../assets/Logo";
+import NavMenu from "./NavMenu/NavMenu";
+import Account from "./Account/Account";
+import styles from "./Header.module.css";
+import useMobile from "../../Hooks/useMobile";
+import MobileMenu from "./MobileMenu/MobileMenu";
 
 const Header = () => {
+  const mobile = useMobile("767px");
+  
   return (
     <header className={styles.Header}>
-      <Logo/>
-      <OptionsMenu/>
-      <Account onClick={(e)=> e.preventDefault()}/>
+      <Logo />
+      {mobile ? (
+        <MobileMenu />
+      ) : (
+        <div className={styles.divNavs}>
+          <NavMenu />
+          <Account onClick={(e) => e.preventDefault()} />
+        </div>
+      )}
     </header>
-  )
-}
+  );
+};
 
 export default Header;
