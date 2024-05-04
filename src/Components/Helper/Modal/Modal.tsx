b1fc605a8684cfb1ecf525d181ae9ca00d6cecd0
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-04 19:44:10 -0300

    refactor: modificado a func e style para opcionais, dando mais liberdade no controle do componente

--- src/Components/Helper/Modal/Modal.tsx
+++ src/Components/Helper/Modal/Modal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Modal.module.css';
 
 type ModalType = {
-  func: React.Dispatch<React.SetStateAction<boolean>>,
+  func?: React.Dispatch<React.SetStateAction<boolean>>,
   stylesFrom?: React.CSSProperties
 }
 
@@ -11,7 +11,7 @@ const Modal = ({func, stylesFrom}: ModalType) => {
     <div
       style={stylesFrom || {}}
       className={styles.Modal}
-      onClick={()=> func((state)=> !state)}
+      onClick={func? ()=> func((state)=> !state) : undefined}
     > 
     </div>
   )
