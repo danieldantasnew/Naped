d63e024f706569bb7dc1ca3c307799b987b48373
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-05 19:18:31 -0300

    feat: adicionado novo estilo e nova section ao home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import Section_1 from '../../Components/Sections/Section_1/Section_1';
 import Section_2 from '../../Components/Sections/Section_2/Section_2';
+import Section_3 from '../../Components/Sections/Section_3/Section_3';
 import { useDataContext } from '../../Store/Context/DataContext';
 import { Naped } from '../../Types/types';
 import styles from './Home.module.css';
@@ -18,6 +19,7 @@ const Home = () => {
     <section className={`${styles.Home} animationLeft`}>
       <Section_1 data={highNotice(data)} />
       <Section_2 data={highNotice(data)} />
+      <Section_3 data={highNotice(data)} />
     </section>
   )
 }
