f3606cf5de861fe7a468b522e1fc097724b7b3dd
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-05 20:32:06 -0300

    feat: adicionado nova seção ao home

---
+++ src/Components/Sections/Section_3/Section_3.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Naped } from '../../../Types/types'
+
+const Section_3 = ({data}: {data: Naped[]}) => {
+  return (
+    <div>Section_3</div>
+  )
+}
+
+export default Section_3
