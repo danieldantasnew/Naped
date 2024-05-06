9f9531c7da14c0f1b8307219c6ea5bff838fc7e4
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-06 18:31:08 -0300

    feat: adicionado as funcionalidades do componente

--- src/Components/Sections/Section_3/Section_3.tsx
+++ src/Components/Sections/Section_3/Section_3.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import { Naped } from '../../../Types/types'
+import React from "react";
+import { Naped } from "../../../Types/types";
+import H2 from "../../Helper/H2/H2";
+import Card from "../../Card/Card";
+import styles from "./Section_3.module.css";
 
-const Section_3 = ({data}: {data: Naped[]}) => {
+const Section_3 = ({ data }: { data: Naped[] }) => {
+  const [newData, setNewData] = React.useState<Naped[] | null>(null);
+
+  React.useEffect(() => {
+    if (data) setNewData(data.slice(0, 6));
+  }, [data]);
+
+  if (!newData) return null;
+
   return (
-    <div>Section_3</div>
-  )
-}
+    <section className={styles.Section_3}>
+      <H2 label="Notícias mais recentes" />
+      <div>
+        {newData.map((item) => (
+          <Card
+            key={item.id}
+            id={item.id}
+            images={item.images}
+            title={item.title}
+            info={item.info}
+            classStyle={styles.configCard}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Section_3
+export default Section_3;
