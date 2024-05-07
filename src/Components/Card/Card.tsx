2b3734e7e75322b6e7508b6604d5cafe67819c4a
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-07 12:53:30 -0300

    refactor: agora card tem uma opção padrão para casos em que o primeiro card não seja grande

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,15 +1,7 @@
-import React from "react";
 import styles from "./Card.module.css";
 import Modal from "../Helper/Modal/Modal";
 import { useNavigate } from "react-router-dom";
 
-const styleModal: React.CSSProperties = {
-  position: "absolute",
-  height: "100%",
-  width: "100%",
-  zIndex: "1",
-  backgroundColor: "#28283061",
-};
 
 type typeCard = {
   id: number;
@@ -18,9 +10,10 @@ type typeCard = {
   activeFunction?: boolean;
   info?: string;
   classStyle?: CSSModuleClasses[string];
+  noLargeFirstCard?: boolean;
 };
 
-const Card = ({ id, title, images, info, classStyle, activeFunction = true }: typeCard) => {
+const Card = ({ id, title, images, info, classStyle, activeFunction = true, noLargeFirstCard = true }: typeCard) => {
   const navigate = useNavigate();
 
   function openPage(id: number) {
@@ -30,11 +23,12 @@ const Card = ({ id, title, images, info, classStyle, activeFunction = true }: ty
   return (
     <div
       className={`
-        ${styles.imageContent} 
+        ${styles.imageContent}
+        ${noLargeFirstCard ? styles.noLargeFirstCard : ''} 
         ${classStyle} 
         `}
       onClick={activeFunction ? () => openPage(id): ()=> ''}>
-        <Modal stylesFrom={styleModal} />
+        <Modal stylesFrom={styles.configModal} />
         <img src={images[0]} alt={`Foto de ${title}`} />
         <h2>{title}</h2>
         {info ? <p>{info}</p> : ""}
