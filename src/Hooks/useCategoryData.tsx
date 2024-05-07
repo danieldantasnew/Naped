9012b1c04c610856f374ae0d0dc1acdac5aea9c3
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-07 17:48:34 -0300

    feat: adicionado nova funcionalidade ao Hook, agora é possível filtrar a categoria e dentro dessa lista de categoria filtrar por um nome específico

--- src/Hooks/useCategoryData.tsx
+++ src/Hooks/useCategoryData.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import { Category, Naped } from '../Types/types';
 
-function filterByCategory(arrayData: Naped[], category: Category) {
+function filterByCategory(arrayData: Naped[], category: Category, search?: string) {
+  if (search) {
+    const escapedContentSearch = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const regex = new RegExp(escapedContentSearch, 'i');
+    return arrayData
+      .filter((item) => item.category === category)
+      .filter((item) => regex.test(item.title));
+  } 
   return arrayData.filter((item)=> item.category === category);
 }
 
-const useCategoryData = (data: Naped[] | null, category: Category) => {
+const useCategoryData = (data: Naped[] | null, category: Category, search?: string) => {
   const [ dataCategory, setDataCategory ] = React.useState<Naped[] | null>(null);
   
   React.useEffect(() => {
     if (data && category) {
-      setDataCategory(filterByCategory(data, category))
+      if (search) setDataCategory(filterByCategory(data, category, search));
+      else setDataCategory(filterByCategory(data, category));
     }
-  }, [data, category]);
+  }, [data, category, search]);
 
   return dataCategory;
 }
