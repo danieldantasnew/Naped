f9bed3f0ad347a360210d9c852285cfe12fc9e69
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-07 18:30:25 -0300

    fix: corrigido bug na sincronização da animação e alterado o efeito para fade in fade out

--- src/Components/Cover/Cover.tsx
+++ src/Components/Cover/Cover.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './Cover.module.css';
 import Modal from '../Helper/Modal/Modal';
 import H2 from '../Helper/H2/H2';
@@ -13,13 +14,24 @@ type CoverType = {
 
 const Cover = ({ title, description, image, slideStart, slideEnd }: CoverType) => {
   const slide = useAutoSlideTransition(slideStart, slideEnd);
+  const [animationDelay, setAnimationDelay] = React.useState(false);
+
+  React.useEffect(() => {
+    setAnimationDelay(true);
+    const timeOutToAnimate = setTimeout(() => {
+      setAnimationDelay(false)
+    }, 8000);
+    
+    return () => clearTimeout(timeOutToAnimate);
+  }, [slide]);
 
   return (
     <div className={styles.Cover}>
-      <div className={styles.imageContent}>
-        <Modal
-          stylesFrom={styles.configModal}
-        />
+      <div
+        className={`${styles.imageContent} ${animationDelay ? styles.animationDelay : ''}`}>
+          <Modal
+            stylesFrom={styles.configModal}
+          />
         <img src={image[slide]} alt={`Capa da categoria ${title}`} />
       </div>
       <H2 label={title} classStyle={styles.configH2} />
