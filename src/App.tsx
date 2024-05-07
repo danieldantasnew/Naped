84afbb7e12215ed6306149f1103d90a78d7155dd
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-07 12:50:42 -0300

    feat: adicionado novas rotas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/Home/Home";
 import {DataContext} from "./Store/Context/DataContext";
 import ItemPage from "./Pages/ItemPage/ItemPage";
+import Series from "./Pages/Series/Series";
+import Movies from "./Pages/Movies/Movies";
+import Animes from "./Pages/Animes/Animes";
+import Games from "./Pages/Games/Games";
 
 function App() {
   return (
@@ -15,6 +19,10 @@ function App() {
           <main>
             <Routes>
               <Route path="/" element={<Home />}></Route>
+              <Route path="/series" element={<Series/>}></Route>
+              <Route path="/movies" element={<Movies/>}></Route>
+              <Route path="/animes" element={<Animes/>}></Route>
+              <Route path="/games" element={<Games/>}></Route>
               <Route path="/item/:id" element={<ItemPage/>}></Route>
               <Route path="*" element={<div>non se</div>}></Route>
             </Routes>
