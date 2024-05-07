b989cb4018e633864f84799647e32f90fe39a5c7
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-07 06:02:21 -0300

    feat: adicionado ordem por mais recentes

--- src/Components/Sections/Section_3/Section_3.tsx
+++ src/Components/Sections/Section_3/Section_3.tsx
@@ -3,12 +3,29 @@ import { Naped } from "../../../Types/types";
 import H2 from "../../Helper/H2/H2";
 import Card from "../../Card/Card";
 import styles from "./Section_3.module.css";
+import StringToDate from "../../../Functions/StringToDate/StringToDate";
+
+function sortByRecents(dataCome: Naped[]) {
+  const dataConvert = dataCome.sort((itemA, itemB) => {
+    const dateA = StringToDate(itemA.date)?.getTime();
+    const dateB = StringToDate(itemB.date)?.getTime();
+
+    if (!dateA || !dateB) return 0;
+
+    return dateB - dateA;
+  });
+
+  return dataConvert;
+}
 
 const Section_3 = ({ data }: { data: Naped[] }) => {
   const [newData, setNewData] = React.useState<Naped[] | null>(null);
 
   React.useEffect(() => {
-    if (data) setNewData(data.slice(0, 6));
+    if (data) {
+      const dataConverted = sortByRecents(data.slice()).slice(0, 6);
+      setNewData(dataConverted);
+    }
   }, [data]);
 
   if (!newData) return null;
