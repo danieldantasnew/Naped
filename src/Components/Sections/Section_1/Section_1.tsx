406da3757087c4bb573070f1dcfc1f48c99154fe
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-07 13:00:23 -0300

    style: alterado formado do código

--- src/Components/Sections/Section_1/Section_1.tsx
+++ src/Components/Sections/Section_1/Section_1.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import styles from './Section_1.module.css';
-import { Naped } from '../../../Types/types';
-import Card from '../../Card/Card';
+import React from "react";
+import styles from "./Section_1.module.css";
+import { Naped } from "../../../Types/types";
+import Card from "../../Card/Card";
 
 const Section_1 = ({ data }: { data: Naped[] }) => {
   const [newData, setNewData] = React.useState<Naped[] | null>(null);
-  
+
   React.useEffect(() => {
     if (data) {
-      setNewData(data.slice(0,3));
+      setNewData(data.slice(0, 3));
     }
-  }, [data]); 
+  }, [data]);
 
   if (!newData) return null;
 
@@ -18,15 +18,26 @@ const Section_1 = ({ data }: { data: Naped[] }) => {
     <section className={styles.Section_1}>
       <div className={styles.content_1}>
         <h1>Mundo nerd? Naped!</h1>
-        <p>O Naped pode ser sua fonte de informações sobre o mundo nerd e outros assuntos relacionados.</p>
+        <p>
+          O Naped pode ser sua fonte de informações sobre o mundo nerd e outros
+          assuntos relacionados.
+        </p>
       </div>
       <div className={styles.content_2}>
-        {newData.map((item) => 
-          <Card id={item.id} key={item.id} title={item.title} images={item.images} info={item.info} classStyle={styles.imageContent} />
-        )}
+        {newData.map((item) => (
+          <Card
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            images={item.images}
+            info={item.info}
+            classStyle={styles.imageContent}
+            noLargeFirstCard={false}
+          />
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Section_1;
