7735b24eaad2a3487abbde2d522be1b0513245e2
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-07 07:28:32 -0300

    feat: adicionado tela de carregamento

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Loading from '../../Components/Helper/Loading/Loading';
 import Section_1 from '../../Components/Sections/Section_1/Section_1';
 import Section_2 from '../../Components/Sections/Section_2/Section_2';
 import Section_3 from '../../Components/Sections/Section_3/Section_3';
@@ -14,7 +15,7 @@ const highNotice = (data: Naped[]) => {
 const Home = () => {
   const { data, loading } = useDataContext();
 
-  if(loading) return <div>Carregando...</div>
+  if (loading) return <Loading />;
   if (!data) return null;
   return (
     <section className={`${styles.Home} animationLeft`}>
