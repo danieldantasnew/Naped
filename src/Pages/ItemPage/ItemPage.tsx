5bdab4bd01e2c643be742375786f515c7cb63d9b
Daniel Dantas <83767201+danieldantasnew@users.noreply.github.com>
2024-05-07 07:29:18 -0300

    feat: adicionado estrutura inicial

--- src/Pages/ItemPage/ItemPage.tsx
+++ src/Pages/ItemPage/ItemPage.tsx
@@ -2,26 +2,53 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "../../Hooks/useFetch";
 import { Naped } from "../../Types/types";
+import StringToDate from "../../Functions/StringToDate/StringToDate";
+import FormatDate from "../../Functions/NormalizeDate/formatDate";
+import Card from "../../Components/Card/Card";
+import styles from "./ItemPage.module.css";
+import Loading from "../../Components/Helper/Loading/Loading";
 
 const ItemPage = () => {
   const { id } = useParams();
-  const fetchData = useFetch<Naped>(`http://localhost:3000/naped/${id}`);
+  const [newDate, setNewDate] = React.useState<string | null>(null);
+  const {data, loading, error} = useFetch<Naped>(`http://localhost:3000/naped/${id}`);
+  
 
   React.useEffect(() => {
-    if (fetchData.data) {
+    if (data) {
       fetch(`http://localhost:3000/naped/${id}`, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          clicks: fetchData.data.clicks + 1,
+          clicks: data.clicks + 1,
         }),
       });
+      const dateConverted = StringToDate(data.date);
+      if(dateConverted) setNewDate(FormatDate(dateConverted, true));
     }
-  }, [fetchData, id]);
+  }, [data, id]);
 
-  return <div>ItemPage</div>;
+
+  if (loading) return <Loading />;
+  if(!data) return null;
+
+  return (
+    <section className={`${styles.ItemPage} animationLeft`}>
+      <h2>{data.info}</h2>
+      <p>{data.moreInfo}</p>
+      <p>Publicado em {newDate}</p>
+      <Card
+        key={data.id}
+        id={data.id}
+        images={data.images}
+        title={data.title}
+        classStyle={styles.configCard}
+        activeFunction={false}
+      />
+    </section>
+  );
 };
 
 export default ItemPage;
